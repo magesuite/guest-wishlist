140e3a1a02ee658a65da7802d081110843f7fc25
Syrovatskyi Yevhenii <yevhenii.syrovatskyi@smile-ukraine.com>
2025-07-23 09:32:36 +0000

    fix: [NKD-3535] Check item store id

--- Plugin/Wishlist/Model/Item/SkipSaveIfItemExists.php
+++ Plugin/Wishlist/Model/Item/SkipSaveIfItemExists.php
@@ -6,8 +6,11 @@ namespace MageSuite\GuestWishlist\Plugin\Wishlist\Model\Item;
 
 class SkipSaveIfItemExists
 {
+    protected array $sharedStoreIds = [];
+
     public function __construct(
         protected \MageSuite\GuestWishlist\Service\GetWishlistItems $getWishlistItems,
+        protected \Magento\Store\Model\StoreManagerInterface $storeManager,
     ) {
     }
 
@@ -20,17 +23,41 @@ class SkipSaveIfItemExists
         }
         $items = $this->getWishlistItems->execute((int) $subject->getWishlistId(), (int) $subject->getProductId());
         $guestValue = $subject->getOptionByCode('simple_product')?->getValue();
+        $sharedStoreIds = $this->getSharedStoreIds((int) $subject->getStoreId());
 
         foreach ($items as $item) {
             if ($item instanceof \Magento\Wishlist\Model\Item && $item->getId()) {
                 $originValue = $item->getOptionByCode('simple_product')?->getValue();
 
-                if ($guestValue === $originValue) {
+                if ($guestValue === $originValue && in_array($item->getStoreId(), $sharedStoreIds)) {
                     return $item;
                 }
             }
         }
 
         return $proceed();
+    }
+
+    protected function getSharedStoreIds(int $storeId): array
+    {
+        if (isset($this->sharedStoreIds[$storeId])) {
+            return $this->sharedStoreIds[$storeId];
+        }
+        $this->sharedStoreIds[$storeId] = [];
+
+        try {
+            /** @var \Magento\Store\Model\Store $store */
+            $store = $this->storeManager->getStore($storeId);
+            $storeIds = $store->getWebsite()->getStoreIds();
+            $this->sharedStoreIds[$storeId] = $storeIds;
+
+            foreach ($storeIds as $entityId) {
+                $this->sharedStoreIds[(int) $entityId] = $storeIds;
+            }
+        } catch (\Exception $e) {
+            return [];
+        }
+
+        return $this->sharedStoreIds[$storeId];
     }
 }
